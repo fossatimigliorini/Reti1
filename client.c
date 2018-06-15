2e7e3b685d89e24c595cde81a749fd4dfdc69ab4
fossatimigliorini <40268346+fossatimigliorini@users.noreply.github.com>
2018-06-15 17:17:47 +0200

    Create client.c

---
+++ client.c
@@ -0,0 +1,121 @@
+#include <stdio.h>      
+#include <sys/types.h>
+#include <sys/socket.h>   
+#include <netdb.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define dim 5
+
+int main(int argc, char *argv[]) {
+
+    int simpleSocket = 0;
+    int simplePort = 0;
+    int returnStatus = 0;
+    char buffer[256] = "";
+    struct sockaddr_in simpleServer;
+    
+    if (3 != argc) {
+
+        fprintf(stderr, "Usage: %s <server> <port>\n", argv[0]);
+        exit(1);
+
+    }
+
+    /* create a streaming socket      */
+    simpleSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (simpleSocket == -1) {
+
+        fprintf(stderr, "Could not create a socket!\n");
+        exit(1);
+
+    }
+    else {
+	    fprintf(stderr, "Socket created!\n");
+    }
+
+    /* retrieve the port number for connecting */
+    simplePort = atoi(argv[2]);
+
+    /* setup the address structure */
+    /* use the IP address sent as an argument for the server address  */
+    //bzero(&simpleServer, sizeof(simpleServer)); 
+    memset(&simpleServer, '\0', sizeof(simpleServer));
+    simpleServer.sin_family = AF_INET;
+    //inet_addr(argv[2], &simpleServer.sin_addr.s_addr);
+    simpleServer.sin_addr.s_addr=inet_addr(argv[1]);
+    simpleServer.sin_port = htons(simplePort);
+
+    /*  connect to the address and port with our socket  */
+    returnStatus = connect(simpleSocket, (struct sockaddr *)&simpleServer, sizeof(simpleServer));
+
+    if (returnStatus == 0) {
+	    fprintf(stderr, "Connect successful!\n");
+    }
+    else {
+        fprintf(stderr, "Could not connect to address!\n");
+	    close(simpleSocket);
+	    exit(1);
+    }
+
+
+
+
+		
+		char id[dim]="";
+		
+		printf("Buongiorno!\n Inserire identificativo valido (compreso tra 1 e 20)\n");
+		//do{
+		
+			fgets(id,dim,stdin);
+			
+		/*	if(id < 1 || id > 20){
+			
+				printf("Errore, inserisci un numero compreso tra 1 e 20");
+				
+			}	
+			
+		}while(id < 1 || id > 20);
+	*/
+
+		//Invio ID al Server
+		
+		int sendId = 0;
+		
+		sendId = write (simpleSocket,id,strlen(id));
+
+
+		//Ricezione ID dal Server
+		int lett;
+		
+		lett = read(simpleSocket, id, dim);
+        printf("OK Benvenuto Utente %s alla Bacheca Messaggi \n",id);
+		
+
+
+
+
+
+
+    /* get the message from the server   */
+    returnStatus = read(simpleSocket, buffer, sizeof(buffer));
+
+    if ( returnStatus > 0 ) {
+        printf("%d: %s", returnStatus, buffer);
+    } else {
+        fprintf(stderr, "Return Status = %d \n", returnStatus);
+    }   
+    
+    
+
+
+    
+    /* send the message to the server */
+
+        close(simpleSocket);
+    
+    return 0;
+}
